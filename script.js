7d305a7bb1ffdc89c2913de23ed6b136133327eb
Prakhar Singh <prakharpk2412@gmail.com>
2026-06-22 10:39:21 +0530

    Made changes

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("featured").addEventListener("click", function () {
-    window.location.href = "featured.html"
-});
-
-document.getElementById("foryou").addEventListener("click", function () {
-    window.location.href = "medium.html"
-});
