97dc98094d8a6454465557625991cd8cd29f4105
Prakhar Singh <prakharpk2412@gmail.com>
2026-06-22 10:20:49 +0530

    Updated files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("featured").addEventListener("click", function () {
+    window.location.href = "featured.html"
+});
+
+document.getElementById("foryou").addEventListener("click", function () {
+    window.location.href = "medium.html"
+});
